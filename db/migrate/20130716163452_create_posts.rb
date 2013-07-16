13c71e19e45e9543fa59124519232731c0ae4ab6
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-16 16:47:10 -0500

    Update db schema

--- db/migrate/20130716163452_create_posts.rb
+++ db/migrate/20130716163452_create_posts.rb
@@ -1,4 +1,11 @@
 class CreatePosts < ActiveRecord::Migration
   def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :text
+      t.belongs_to :categories
+      
+      t.timestamps
+    end
   end
 end
--- db/migrate/20130716163516_create_categories.rb
+++ db/migrate/20130716163516_create_categories.rb
@@ -1,5 +1,10 @@
 class CreateCategories < ActiveRecord::Migration
   def change
-    
+    create_table :categories do |t|
+      t.string :title
+      t.string :description
+      
+      t.timestamps
+    end
   end
 end
